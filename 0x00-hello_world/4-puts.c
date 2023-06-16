41ee67138baed519f3817d7e740e9684337fea42
collins ibeh <ibehcollins@gmail.com>
2023-06-16 16:50:15 -0700

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
+
 /**
- * main -prints "Programming is like building a multilingual puzzle, 
- * followed by a new line."
+ * main - prints "Programming is like building a multilingual
+ * puzzle, followed by a new line.
  * Return: 0.
  */
 int main(void)
 
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
