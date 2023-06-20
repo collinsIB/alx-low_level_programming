249a0bc7d959ad5fafc01f606e80ce16b800a76a
collins ibeh <ibehcollins@gmail.com>
2023-06-19 23:52:23 -0700

    4 print some alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  * main - Prints letters except e and q
+  *
+  * Return: 0 (Success)
+  */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
