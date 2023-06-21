2c1873136fe05c6daf10e967bbf61ac1283bef48
collins ibeh <ibehcollins@gmail.com>
2023-06-21 02:46:21 -0700

    6 abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of integar
+ * @n: int to check
+ *
+ * Return: value of int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
