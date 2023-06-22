e5687d9db8673092f6938d433056e8d4c548207b
collins ibeh <ibehcollins@gmail.com>
2023-06-22 07:43:14 -0700

    2 mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiple of two int
+ * @a: the first number to multiply
+ * @b: the second number to multiply
+ *
+ * Return: result a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
