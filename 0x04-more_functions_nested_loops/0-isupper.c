94c3344e59bfc9bbfb5972041dae1cf256d75d62
collins ibeh <ibehcollins@gmail.com>
2023-06-22 07:24:08 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * _isupper - function that checks for upper case char
+ *
+ * @c: the file to check
+ * Return: 1 for uppercase or 0 for any else
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'B')
+	{
+		return (1);
+	}
+
+	return (0);
+}
