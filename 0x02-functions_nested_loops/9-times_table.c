2b883751ff73c54625a71187aea1093699b5ad65
collins ibeh <ibehcollins@gmail.com>
2023-06-21 20:36:48 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,31 +9,31 @@ void times_table(void)
 {
 	int i, k, l;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (k = 0; k < 10; k++)
+		for (k = 0; k <= 9; k++)
 		{
-			l =  k * i;
+			l = i * k;
 			if (k == 0)
 			{
+				_putchar('0')
+			}
+			else if (l <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 				_putchar(l + '0');
 			}
-
-			if (l < 10 && k != 0)
+			else
 			{
 				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(l + '0');
-			} else if (l > 10)
-			{
-				_putchar(',');
-				_putchar(' ');
 				_putchar(' ');
 				_putchar((l / 10) + '0');
 				_putchar((l % 10) + '0');
+				
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
