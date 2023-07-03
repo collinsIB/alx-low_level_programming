a5dbc7e10c89721ec3378559db911d553204f3a6
collins ibeh <ibehcollins@gmail.com>
2023-07-03 13:50:02 -0700

    4-strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 
 /**
  * _strpbrk - searches a string
@@ -13,6 +13,7 @@
 char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
