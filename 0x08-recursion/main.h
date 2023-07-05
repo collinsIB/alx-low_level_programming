68c637b222b1bf6718a8488894f449e149cdbb40
collins ibeh <ibehcollins@gmail.com>
2023-07-05 16:36:49 -0700

    6-is_prime_numbers

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -9,4 +12,4 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
-
+#endif
