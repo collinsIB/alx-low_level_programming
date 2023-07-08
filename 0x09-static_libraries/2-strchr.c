c27ae37d730f411f8a82a80487df472628b70de7
collins ibeh <ibehcollins@gmail.com>
2023-07-08 04:19:26 -0700

    static_libraries

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,20 +1,24 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
- * _strchr - Entry point
- * @s: input
- * @c: input
- * Return: Always 0 (Success)
+ * _strchr - function that locates a character in a string
+ * @s: string
+ * @c: first occurence of the character
+ *
+ * Return:pointer to the first occurence
+ * or NULL if the charcter is not found
  */
+
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+	int i;
 
-
-	for (; s[i] >= '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
-			return (&s[i]);
+			return (s + i);
 	}
-	return (0);
-}
 
+	return (NULL);
+}
