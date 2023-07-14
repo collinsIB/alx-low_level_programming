36f8348d140f84921758dfd940ed5ebac269bd36
collins ibeh <ibehcollins@gmail.com>
2023-07-13 17:12:43 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,47 +1,36 @@
 #include <stdlib.h>
 #include "main.h"
-#include <stdio.h>
+#include <string.h>
 
 /**
  * string_nconcat - function that concatenate two strings
  * @s1: first strings
  * @s2: second strings
+ * @n: int value
  *
  * Return: NULL
  */
 
-char *string_nconcat(char *s1, char *s2)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *conc;
-	int j, k;
+	unsigned int len1, len2, i, j;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-	j = k = 0;
-	while (s1[j] != '\0')
-		j++;
-	while (s2[k] != '\0')
-		k++;
-
-	conc = malloc(sizeof(char) * (j + k + 1));
-
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	if (n > len2)
+		n = len2;
+	conc = malloc(sizeof(char) * (len1 + n + 1));
 	if (conc == NULL)
 		return (NULL);
-	j = k = 0;
-	while (s1[j] != '\0')
-	{
-		conc[j] = s1[j];
-			j++;
-	}
-		while (s2[k] != '\0')
-		{
-			conc[j] = s2[k];
-			j++, k++;
-		}
-		conc[j] = '\0';
-		return (conc);
+	for (i = 0; i < len1; i++)
+		conc[i] = s1[i];
+	for (j = 0; j < n; j++, i++)
+		conc[i] = s2[j];
+	conc[i] = '\0';
+	return (conc);
 }
-
