1fdfd591470df4003899d16d87fc7030c5e62eb6
collins ibeh <ibehcollins@gmail.com>
2023-07-17 15:41:02 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the name of the file
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("name of the file%s\n", __FILE__);
+	return (0);
+}
