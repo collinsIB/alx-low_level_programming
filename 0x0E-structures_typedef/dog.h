c39beb839db82ee1b2806804b2d0ac59232f9ab5
collins ibeh <ibehcollins@gmail.com>
2023-07-18 14:45:43 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - define a new type struct dog
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ *
+ * Return: 0
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
