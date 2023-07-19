d38f25cdd6721f76fc420b24734c5b20eec2709f
collins ibeh <ibehcollins@gmail.com>
2023-07-19 16:03:13 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - a function hat prints a name
+ * @name: name to print
+ * @f: function character
+ * Return: 0
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL ||f == NULL)
+		return;
+	f(name);
+}
