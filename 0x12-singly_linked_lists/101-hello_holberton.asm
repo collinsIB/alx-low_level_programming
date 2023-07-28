bee3f4055a08c9dfb19d66b6614b5ae7d7600551
collins ibeh <ibehcollins@gmail.com>
2023-07-28 03:25:16 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,9 @@
-; File: 101-hello_holberton.asm
-; Assembly code to print "Hello, Holberton"
 	global main
 	extern printf
-	
-	section .data
-msg:	db	'Hello, Holberton', 0
-	section .text
-
-main:	
-; prepare arguments for printf
-	push	rbp
-	mov	rdi, msg
-	xor	eax, eax
-	call	printf
-	
-	; clean up stack and return
-	pop	rdp
+main:
+	mov edi, format
+	xor eax, eax
+	call printf
+	mov eax, 0
 	ret
+format: db 'Hello, Holberton\n', 0
