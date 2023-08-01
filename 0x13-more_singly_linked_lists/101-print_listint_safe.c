8eb647c0f421e2d5126bd56fe00bf779dc576266
collins ibeh <ibehcollins@gmail.com>
2023-08-01 15:13:31 -0700

    update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,47 @@
 #include "lists.h"
 
+size_t looped_listint_len(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
+
+/**
+ * looped_listint_len - counts number of unique codes
+ * @head: a pointer to the head
+ *
+ * Return: if the list is not looped 0
+ */
+size_t looped_listint_len(const listint_t *head)
+{
+	const listint_t *c, *b;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	c = head->next;
+	b = (head->next)->next;
+
+	while (b)
+	{
+		if (c == b)
+		{
+			c = head;
+
+			while (c != b)
+			{
+				nodes++;
+				c = c->next;
+			}
+
+			return (nodes);
+		}
+
+		c = c->next;
+		b = (b->next)->next;
+	}
+
+	return (0);
+}
+
 /**
  * print_listint_safe - prints a listint_t linked list
  * @head: pointer to the first node of the list
