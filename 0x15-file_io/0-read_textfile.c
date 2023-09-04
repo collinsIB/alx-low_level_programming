2a8a02fb407957c7ce48d0f24a23710ff68664d1
collins ibeh <ibehcollins@gmail.com>
2023-09-04 10:40:30 -0700

    1-create_file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that reads a text fie and prints to POSIX
+ * @filename: name of the file to read
+ * @letters: number of letters using the read system call
+ *
+ * Return: 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t n_read, n_written;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	n_read = read(fd, buffer, letters);
+	if (n_read == -1)
+		return (0);
+
+	n_written = write(STDOUT_FILENO, buffer, n_read);
+	if (n_written == -1 || n_written != n_read)
+		return (0);
+
+	close(fd);
+	free(buffer);
+
+	return (n_written);
+}
