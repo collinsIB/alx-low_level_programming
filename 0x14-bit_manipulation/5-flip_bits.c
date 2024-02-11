0f9edf416dcd75b84a37f271230f44ca2ca7336b
collins ibeh <ibehcollins@gmail.com>
2024-02-10 16:57:14 -0800

    bit manipulation updated

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - function that returns the number of bits
+ * @n: unsigned long int
+ * @m: value input
+ *
+ * Return: number of bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor = n ^ m;
+	unsigned int count = 0;
+
+	while (xor)
+	{
+		count += xor & 1;
+		xor >>= 1;
+	}
+	return (count);
+}
